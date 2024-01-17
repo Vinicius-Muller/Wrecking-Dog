b955b5b05a380713bc850b69956bdc03f7aa8825
Vinicius-Muller <viniciusmul98@gmail.com>
2024-01-17 18:10:01 -0300

    climbing enemies added

--- js/enemies/index.js
+++ js/enemies/index.js
@@ -77,5 +77,33 @@ export class ClimbingEnemy extends Enemy {
   constructor(game) {
     super();
     this.game = game;
+    this.width = 120;
+    this.height = 144;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.image = document.getElementById("enemyBigSpider");
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+    this.maxFrame = 5;
+  };
+  update(deltaTime) {
+    super.update(deltaTime);
+    if(
+      this.y > this.game.height - 
+      this.height - 
+      this.game.groundMargin
+    ) {
+      this.speedY *= -1;
+    }
+    if(this.y < -this.height) {
+      this.markedToDeletion = true;
+    }
+  };
+  draw(context) {
+    super.draw(context);
+    context.beginPath();
+    context.moveTo(this.x + this.width/2, 0);
+    context.lineTo(this.x + this.width/2, this.y + 50);
+    context.stroke();
   }
 };
--- js/index.js
+++ js/index.js
@@ -49,8 +49,9 @@ window.addEventListener("load", () => {
     addEnemy() {
       if(this.speed > 0 && Math.random() < 0.5) {
         this.enemies.push(new GroundEnemy(this))
-      }
+      } else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
       this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies)
     }
   }
 
